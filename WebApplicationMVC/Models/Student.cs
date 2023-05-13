9ee9ec4d5fa6cfa7506959989dfcca00fcbd4ebf
ApogeeX <emil.palczynski@gmail.com>
2023-05-13 16:54:23 +0200

    Koniec lekcji

---
+++ WebApplicationMVC/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationMVC.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string Name { get; set; } = default!;
+
+        [EmailAddress]
+        public string Email { get; set; } = default!;
+        public string Country { get; set; } = default!;
+        public List<SelectListItem> Countries { get; } = new List<SelectListItem>
+        {
+            new SelectListItem {Value="PL", Text="Poland"},
+            new SelectListItem {Value="US", Text="United States of America"},
+            new SelectListItem {Value="DE", Text="Germany"},
+            new SelectListItem {Value="UK", Text="United Kingdom"},
+        };
+    }
+}
