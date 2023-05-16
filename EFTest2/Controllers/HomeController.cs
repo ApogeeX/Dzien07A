2ac90f52a590af87b986cb613c9b86369c711b6e
ApogeeX <emil.palczynski@gmail.com>
2023-05-16 12:38:16 +0200

    Praca domowa skonczona

--- EFTest2/Controllers/HomeController.cs
+++ EFTest2/Controllers/HomeController.cs
@@ -25,9 +25,92 @@ namespace EFTest2.Controllers
             return View(employees);
         }
 
+        public IActionResult Details(int id)
+        {
+            var employee = _dbContext.Employees.Where(x => x.Id == id).FirstOrDefault();
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var employee = _dbContext.Employees.Where(x => x.Id == id).FirstOrDefault();
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee employee)
+        {
+            //update
+            var p = _dbContext.Employees.Where(x => x.Id == employee.Id).FirstOrDefault();
+            if (p == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Employees.Remove(p);
+            _dbContext.Employees.Add(employee);
+            //_dbContext.Employees.Add(new Employee());
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var p = _dbContext.Employees.Where(x => x.Id == id).FirstOrDefault();
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return View(p);
+
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(Employee employee)
+        {
+            var p = _dbContext.Employees.Where(x => x.Id == employee.Id).FirstOrDefault();
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Employees.Remove(p);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Create()
+        {
+            int x = 0;
+            if (_dbContext.Employees.Count() == 0)
+            {
+                x = 1;
+            }
+            else
+            {
+                x = _dbContext.Employees.Max(x => x.Id) + 1;
+            }
+            var p = new Employee { Id = x, Email = "", Name = "", Address="" };
+            return View(p);
+        }
+
+        public IActionResult CreateConfirmed(Employee employee)
+        {
+            _dbContext.Employees.Add(employee);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
